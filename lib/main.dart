82984877c33f2847d631633f9a439458a49db242
Mizael Eduardo <eduardo_boyfla@hotmail.com>
2022-06-23 18:40:01 -0300

    Craindo login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/pages/home_page/home_page.dart';
+import 'package:instagram_clone/pages/login_page/login_page.dart';
 import 'package:instagram_clone/responsive/mobile_page_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_page.dart';
 import 'package:instagram_clone/responsive/web_page_layout.dart';
-import 'package:instagram_clone/utils/app_routes/app_routes.dart';
 import 'package:instagram_clone/utils/colors/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAJQN0Is-bbJ1ZMtoRh-T05LsAivWD4-zc',
+        appId: '1:124901356060:web:d7fd179c88c0eb4d90a5d9',
+        messagingSenderId: '124901356060',
+        projectId: 'instagram-clone-eabdb',
+        storageBucket: 'instagram-clone-eabdb.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -20,10 +35,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const ResponsiveLayout(
+      home: const LoginPage(),
+      /* home: const ResponsiveLayout(
         mobilePageLayout: MobilePageLayout(),
         webPageLayout: WebPageLayout(),
-      ),
+      ), */
       /* routes: {
         AppRoutes.HOME_PAGE: (ctx) => const HomePage(),
       }, */
---
+++ lib/pages/login_page/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('From login page'),
+        ),
+      ),
+    );
+  }
+}
