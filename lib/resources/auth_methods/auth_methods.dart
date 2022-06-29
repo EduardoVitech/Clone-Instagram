719c4a814e8d302b72759e259e8782f0fa695350
Mizael Eduardo <eduardo_boyfla@hotmail.com>
2022-06-29 16:35:34 -0300

    Autenticação prt 1

---
+++ lib/resources/auth_methods/auth_methods.dart
@@ -0,0 +1,38 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //Sign up user
+  Future<String> SignUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    required Uint8List file,
+  }) async {
+    String res = "Some error occurred";
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty ||
+          file != null) {
+        //register user
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        // add user to our database
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}
