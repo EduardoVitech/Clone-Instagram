593f15b5d81f0abcbf2359be1cf8efaaf4854b98
Mizael Eduardo <eduardo_boyfla@hotmail.com>
2022-07-17 19:01:54 -0300

    Comment new update

--- lib/pages/comment_page/comment_page.dart
+++ lib/pages/comment_page/comment_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/models/user/user.dart';
 import 'package:instagram_clone/providers/user/user_providder.dart';
@@ -16,6 +17,14 @@ class CommentPage extends StatefulWidget {
 }
 
 class _CommentPageState extends State<CommentPage> {
+  final TextEditingController _commentController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _commentController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context).getUser;
@@ -26,7 +35,25 @@ class _CommentPageState extends State<CommentPage> {
         title: const Text('Comments'),
         centerTitle: false,
       ),
-      body: CommentCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('posts')
+            .doc(widget.snap['postId'])
+            .collection('comments')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: (snapshot.data! as dynamic).docs.length,
+            itemBuilder: (context, index) => const CommentCard(),
+          );
+        },
+      ),
       bottomNavigationBar: SafeArea(
         child: Container(
           height: kToolbarHeight,
@@ -46,6 +73,7 @@ class _CommentPageState extends State<CommentPage> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 16, right: 8),
                   child: TextField(
+                    controller: _commentController,
                     decoration: InputDecoration(
                       hintText: 'Comment as ${user.username}',
                       border: InputBorder.none,
@@ -57,11 +85,14 @@ class _CommentPageState extends State<CommentPage> {
                 onTap: () async {
                   await FirestoreMethods().postComment(
                     widget.snap['postId'],
-                    widget.snap['text'],
+                    _commentController.text,
                     user.uid,
                     user.username,
                     user.photoUrl,
                   );
+                  setState(() {
+                    _commentController.text = "";
+                  });
                 },
                 child: Container(
                   padding: const EdgeInsets.all(8),
--- lib/widgets/post_card/post_card.dart
+++ lib/widgets/post_card/post_card.dart
@@ -167,7 +167,9 @@ class _PostCardState extends State<PostCard> {
               IconButton(
                 onPressed: () => Navigator.of(context).push(
                   MaterialPageRoute(
-                    builder: (context) => const CommentPage(),
+                    builder: (context) => CommentPage(
+                      snap: widget.snap,
+                    ),
                   ),
                 ),
                 icon: const Icon(
